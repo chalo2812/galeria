643711d7160707f2ecb746b8573c8af0ae771b98
Gonzalo Sola <gonzalosola@gmail.com>
2016-10-02 22:18:24 -0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -14,17 +14,21 @@
   <h2>Registrarte</h2>
   <form>
     <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Enter email">
+      <label for="email">Tu Email:</label>
+      <input type="email" class="form-control col-xs-4" id="email" placeholder="Ingrese email" required>
     </div>
     <div class="form-group">
-      <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+      <label for="pwd">Contrase&acute;:</label>
+      <input type="password" class="form-control col-xs-4" id="pwd" placeholder="Ingrese password" required>
+      <span>Minimo 4 caracteres requeridas...</span>
+    </div>
+    <div class="form-group">
+      <label for="pwd">Confirmaci&oacute;n Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Ingrese password">
     </div>
     <div class="checkbox">
-      <label><input type="checkbox"> Remember me</label>
+      <input type="button" class="btn btn-primary btn-md" name="" value="Crear tu usuario">
     </div>
-    <button type="submit" class="btn btn-default">Submit</button>
   </form>
 </div>
 
